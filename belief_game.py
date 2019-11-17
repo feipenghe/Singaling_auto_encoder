26ccad2443ae163f911f43a891afff854313f2d7
nur <nur@airbud.io>
2019-11-17 17:54:35 +0100

    Working belief game

--- belief_game.py
+++ belief_game.py
@@ -79,6 +79,7 @@ def make_belief_update_simulation(
     message_sizes: Tuple[int, ...],
     shared_context: bool,
     use_context: bool,
+    **kwargs,
 ) -> simulations.Simulation:
     return simulations.Simulation(
         name=f"belief_update_game_c{context_size}_o{object_size}_f{num_functions}_m{utils.join_vals(message_sizes)}_sharedcontext{int(shared_context)}",
@@ -90,17 +91,29 @@ def make_belief_update_simulation(
         num_functions=num_functions,
         shared_context=shared_context,
         message_sizes=message_sizes,
+        **kwargs,
     )
 
 
 if __name__ == "__main__":
-    belief_update_simulation = make_belief_update_simulation(
-        context_size=10,
-        object_size=10,
-        num_functions=4,
-        message_sizes=(1, 2, 4, 6, 8, 10),
-        shared_context=True,
-        use_context=True,
-    )
+    # belief_update_simulation = make_belief_update_simulation(
+    #     context_size=10,
+    #     object_size=10,
+    #     num_functions=4,
+    #     message_sizes=(1, 2, 4, 6, 8, 10),
+    #     shared_context=True,
+    #     use_context=True,
+    # )
 
-    games = simulations.run_simulation(belief_update_simulation, visualize=True)
+    simulations.run_simulation_set(
+        "belief_update",
+        make_belief_update_simulation,
+        message_sizes=(1, 2, 4, 6, 8, 10, 12),
+        context_size=(2, 6, 10),
+        object_size=(2, 6, 10),
+        num_functions=(2, 4, 6),
+        shared_context=(True,),
+        use_context=(True,),
+        num_trials=(3,),
+        num_processes=None,
+    )
